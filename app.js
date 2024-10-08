810796255e131c9ea14b5a748b1a3136c567ea1f
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-10-08 11:18:19 +0100

    15:45

--- app.js
+++ app.js
@@ -14,6 +14,4 @@ app.get('/hello', (req, res) => {
 app.use('/api/v1/tasks', tasks);
 
 const port = 3000;
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}...`);
-});
+app.listen(port, console.log(`Server is listening on port ${port}...`));
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,8 +1,1 @@
-// controllers/tasks.js
-const getAllTasks = (req, res) => {
-    res.send('pleaseweeeee mother fucker please'); // Cambia esto a lo que quieras que se muestre
-}
 
-module.exports = {
-    getAllTasks,
-};
