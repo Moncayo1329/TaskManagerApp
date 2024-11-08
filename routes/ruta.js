41ed7f2c516b929d913326eeb79fe54a83f14363
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-11-08 17:09:28 +0000

    postman

--- routes/ruta.js
+++ routes/ruta.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+
+const {getAllTasks,createTask,getTask,updateTask,deleteTask} = require('../controllers/tasks')
+
 // Ruta para manejar las solicitudes GET en /api/v1/tasks
-router.route('/').get((req, res) => {
-    res.send('all items');
-});
+router.route('/').get(getAllTasks).post(createTask)
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
 
 module.exports = router;
 
