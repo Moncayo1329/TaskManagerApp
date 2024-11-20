e14d871fd849c672a12e799178539f3cbd45d767
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-11-20 00:14:14 +0000

    2:23:13

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -80,9 +80,32 @@ const getTask = async (req,res) => {
      }
                } 
 
+   const editTask =  async (req,res) => {
+      try{
+      const {id:taskID} = req.params;
+ 
+      const task = await Task.findOneAndDelete({_id:taskID}, req.body,{
+       new:true,
+       runValidators:true,
+      })
+ 
+ 
+      if(!task){
+       return res.status(404).json({msg:`no task with id : ${taskID}`})
+    }
+ 
+       res.status(200).json({})
+ 
+      } catch(error) {
+       res.status(500).json({msg:error})
+ 
+ 
+      }
+                } 
+
 
 module.exports = {
 
-getAllTasks,createTask,getTask,updateTask,deleteTask
+getAllTasks,createTask,getTask,updateTask,deleteTask,editTask
 
 }
--- routes/ruta.js
+++ routes/ruta.js
@@ -6,7 +6,7 @@ const {getAllTasks,createTask,getTask,updateTask,deleteTask} = require('../contr
 
 // Ruta para manejar las solicitudes GET en /api/v1/tasks
 router.route('/').get(getAllTasks).post(createTask)
-router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask).put(editTask)
 
 module.exports = router;
 
